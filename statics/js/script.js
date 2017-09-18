ab81c63f8e19726e435a0dd4aa69ddc7d36a5cae
SummySumanth <sumanth261094@gmail.com>
2017-09-18 12:55:24 +0530

    js base structure

---
+++ statics/js/script.js
@@ -0,0 +1,19 @@
+let suits = ['Clubs', 'Hearts', 'Spades', 'Diamonds'];
+
+let ranks = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
+
+let hand = [
+				{	rank:'A', suit: 'Clubs'		},
+				{	rank:'A', suit: 'Diamonds'	},
+				{	rank:'9', suit: 'Hearts'	},
+				{	rank:'6', suit: 'Spades'	},
+				{	rank:'4', suit: 'Diamonds'	}
+			];
+
+let containsNTimes = (array, elementToBeSearcher, minimum) =>{
+
+}
+
+let isOnePair = (hand) =>{
+
+}
